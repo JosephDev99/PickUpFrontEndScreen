a69c40c8c20c6089770c3d155bc72541d0a63bdd
YuriAdrian <elfsightcoredev@outlook.com>
2022-07-12 03:56:18 +0900

    Finished building UI for Pick up Location Screen

--- components/LocationInput.tsx
+++ components/LocationInput.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import {
-  View,
-  TextInput,
-  StyleSheet,
   Pressable,
+  StyleSheet,
+  TextInput,
+  View,
 } from "react-native";
-import { SimpleLineIcons, Entypo } from '@expo/vector-icons';
+import { Entypo, SimpleLineIcons } from '@expo/vector-icons';
 
 const LocationInput = (props: any) => {
   return (
-    <>
-      <View style={styles.container}>
-        <TextInput
-          value={props.location}
-          style={styles.input}
-          underlineColorAndroid="black"
-          placeholder={props.placeholder}
-          placeholderTextColor="black"
-          autoCapitalize="none"
-          onChangeText={(text) => props.setLocation(text)}
-        />
-        <SimpleLineIcons name="microphone" size={18} color="lightgrey" />
-        <Pressable onPress={() => props.setLocation("")}>
-          <Entypo name="cross" size={24} color="grey" />
-        </Pressable>
-      </View>
-    </>
+    <View style={styles.container}>
+      <TextInput
+        value={props.location}
+        style={styles.input}
+        underlineColorAndroid="black"
+        placeholder={props.placeholder}
+        placeholderTextColor="black"
+        autoCapitalize="none"
+        onChangeText={(text) => props.setLocation(text)}
+        onFocus={props.focusIn}
+        onBlur={props.focusOut}
+      />
+      <SimpleLineIcons name="microphone" size={18} color="lightgrey" />
+      <Pressable onPress={() => props.setLocation("")}>
+        <Entypo name="cross" size={24} color="grey" />
+      </Pressable>
+    </View>
   );
 };
 
